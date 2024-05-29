4ac74ca46a2b6860b0c5294c2970aad55e04f94e
AqibGhaffar <aqibg73@gmail.com>
2024-05-29 14:54:46 +0500

    test

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,12 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-
-module.exports = defineConfig({
+export default defineConfig({
+  projectId: "7sdu13",
   e2e: {
-    projectId: "n9684w",
     specPattern: "./cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     experimentalStudio: true,
     pageLoadTimeout: 90000,
-    baseUrl:"https://dev-dot-sqiblify.wl.r.appspot.com/",
+    //baseUrl:"https://dev-dot-sqiblify.wl.r.appspot.com/",
     //STAGE_URL:"https://stage-dot-sqiblify.wl.r.appspot.com/",
     //PROD_URL:"https://squibler.io/",
 
@@ -17,11 +16,24 @@ module.exports = defineConfig({
     //baseUrl: "http://localhost:4200",
     viewportWidth: 1000,
     viewportHeight: 660,
+    retries: {
+      runMode: 2,  // Number of retries when running in `cypress run`
+      openMode: 2, // Number of retries when running in `cypress open`
+    },
+    setupNodeEvents(on, config) {
+      const environment = config.env.environment || 'development';
 
-    
-    setupNodeEvents(on, config) { 
+      const urls = {
+        development: "https://dev-dot-sqiblify.wl.r.appspot.com/",
+        staging: "https://stage-dot-sqiblify.wl.r.appspot.com/",
+        production: "https://squibler.io/"
+      };
+
+      config.baseUrl = urls[environment];
+      return config;
       
       // implement node event listeners here
+      
     },
   },
 });
--- cypress/e2e/community_withoutlogin.cy.js
+++ cypress/e2e/community_withoutlogin.cy.js
@@ -1,87 +1,87 @@
 
 describe('Without Login', () => {
-    it('verify that non-logged in users are not able to post', () => {
+  it('verify that non-logged in users are not able to post', () => {
+    // Visit the webpage where the form is located
+    cy.visit('/community')
+    cy.get('.community-section-filters-button-text').should('be.visible').and('not.be.disabled').click()
+    cy.location('pathname').should('eq', '/auth/login')
+    cy.get('h1').contains('Log in');
+
+  })
+  it('verify that non-logged in users are not able to like', () => {
       // Visit the webpage where the form is located
       cy.visit('/community')
-      cy.get('.community-section-filters-button-text').should('be.visible').and('not.be.disabled').click()
+      cy.get('.fa-like').first().should('be.visible').click()
       cy.location('pathname').should('eq', '/auth/login')
       cy.get('h1').contains('Log in');
   
+      
     })
-    it.skip('verify that non-logged in users are not able to like', () => {
-        // Visit the webpage where the form is located
-        cy.visit('/community')
-        cy.get('.fa-like').first().should('be.visible').click()
-        cy.location('pathname').should('eq', '/auth/login')
-        cy.get('h1').contains('Log in');
-    
+
+
+    it('check the newest record using filter', () => {
+      // Visit the webpage where the form is located
+      cy.visit('/community')
+      cy.get('#sortBy').click()
+      cy.get('.mat-selected > .mat-option-text').contains(' Newest ').click()
+      // cy.contains('Support Multiple Languages Expand language').click
+  
+      const divDataArray = [];
+      const div = '.community-post-detail-title';
+      // Store div data in an array
+      cy.get(div).each(($div) => {
+        divDataArray.push($div.text());
+      }).then(() => {
+        // Now divDataArray contains the text of each div
+        // You can apply your filter or other logic here
+        console.log('All div data:', divDataArray);
+  
+        // Get text from the first div
+        cy.get(div).eq(0).invoke('text').then((text) => {
         
-      })
-
-
-      it.skip('check the newest record using filter', () => {
-        // Visit the webpage where the form is located
-        cy.visit('/community')
-        cy.get('#sortBy').click()
-        cy.get('.mat-selected > .mat-option-text').contains(' Newest ').click()
-        cy.contains('Support Multiple Languages Expand language').click
-    
-        const divDataArray = [];
-        const div = '.community-post-detail-title';
-        // Store div data in an array
-        cy.get(div).each(($div) => {
-          divDataArray.push($div.text());
-        }).then(() => {
-          // Now divDataArray contains the text of each div
-          // You can apply your filter or other logic here
-          console.log('All div data:', divDataArray);
-    
-          // Get text from the first div
-          cy.get(div).eq(0).invoke('text').then((text) => {
+          cy.log('Text from the first div:', text);
           
-            cy.log('Text from the first div:', text);
-            
-    // Assert that the selected title exists on the page
-    cy.get(".community-post-detail-title").eq(0).should("contain", text)
+  // Assert that the selected title exists on the page
+  cy.get(".community-post-detail-title").eq(0).should("contain", text)
 
 
-  });
-  })
+});
+})
 })
 
 
-it.skip('check the Oldest record using filter', () => {
-  // Visit the webpage where the form is located
-  cy.visit('/community')
-  cy.get('#sortBy').click()
-  cy.get('.mat-option-text').contains('Oldest').click()
-  // cy.contains('Support Multiple Languages Expand language').click
+it.only('check the Oldest record using filter', () => {
+// Visit the webpage where the form is located
+cy.visit('/community')
+cy.get('#sortBy').click()
+cy.get('.mat-option-text').contains('Oldest').click()
+// cy.contains('Support Multiple Languages Expand language').click
 
-  const divDataArray = [];
-  const div = '.community-post-detail-title';
-  // Store div data in an array
-  cy.get(div).each(($div) => {
-    divDataArray.push($div.text());
-  }).then(() => {
-    // Now divDataArray contains the text of each div
-    // You can apply your filter or other logic here
-    console.log('All div data:', divDataArray);
+const divDataArray = [];
+const div = '.community-post-detail-title';
+// Store div data in an array
+cy.get(div).each(($div) => {
+  divDataArray.push($div.text());
+}).then(() => {
+  // Now divDataArray contains the text of each div
+  // You can apply your filter or other logic here
+  console.log('All div data:', divDataArray);
 
-    // Get text from the first div
-    cy.get(div).eq(0).invoke('text').then((text) => {
+  // Get text from the first div
+  cy.get(div).eq(0).invoke('text').then((text) => {
+  
+    cy.log('Text from the first div:', text);
     
-      cy.log('Text from the first div:', text);
-      
 // Assert that the selected title exists on the page
 cy.get(".community-post-detail-title").eq(0).should("contain", text);
 
 
 cy.get('#sortBy').click()
-  cy.get('.mat-option-text').contains('Oldest').click()
+cy.get('.mat-option-text').contains('Oldest').click()
 
-  cy.get(div).last().invoke('text').then((text) => {
-    
-    cy.log('Text from the last div:', text);
+cy.get(div).last().invoke('text').then((text) => {
+  
+  cy.log('Text from the last div:', text);
 
 
 
@@ -90,118 +90,108 @@ cy.get('#sortBy').click()
 })
 })
 
-it.skip('should allow typing and display the search term and handle no results scenario', () => {
-  cy.visit('/community')
+it('should allow typing and display the search term and handle no results scenario', () => {
+cy.visit('/community')
 
-  const search = 'test';
+const search = 'test';
 
-  cy.get('#search-text')
-    .should('be.visible')
-    .clear()
-    .type(search)
-    .should('have.value', search);
+cy.get('#search-text')
+  .should('be.visible')
+  .clear()
+  .type(search)
+  .should('have.value', search);
 
-  cy.get('.community-section-content').then($results => {
-      cy.wrap($results).each(($result) => {
-        cy.wrap($result).should('contain.text', search);
-        cy.log('Search results found')
-        
-      });
+cy.get('.community-section-content').then($results => {
+    cy.wrap($results).each(($result) => {
+      cy.wrap($result).should('contain.text', search);
+      cy.log('Search results found')
       
     });
-    cy.get('#search-text').clear()
-        cy.get('#search-text').type('invisible')
-        cy.get('#mat-tab-content-0-0').contains('No Results').should('be.visible')
-  });
     
-  it.skip('Status Filter working', () => {
-    cy.visit('/community')
-    cy.get('#statusDropdown').click()
-    cy.get('.mat-option-text').contains('Open').click()
-    cy.get('.community-post').first().contains('OPEN')
-  });    
+  });
+  cy.get('#search-text').clear()
+      cy.get('#search-text').type('invisible')
+      cy.get('#mat-tab-content-0-0').contains('No Results').should('be.visible')
+});
+  
+it('Status Filter working', () => {
+  cy.visit('/community')
+  cy.get('#statusDropdown').click()
+  cy.get('.mat-option-text').contains('Open').click()
+  cy.get('.community-post').first().contains('OPEN')
+});    
 
-//   it.skip('should allow typing and display the search term and handle no results scenario', () => {
-//     // Visit the community page
-//     cy.visit('/community')
-  
-//     // Generate a random search term
-//     const search = generateRandomSearchTerm();
-  
-//     // Type the search term into the input field
-//     cy.get('#search-text')
-//       .should('be.visible')
-//       .clear()
-//       .type(search)
-//       .should('have.value', search);
-  
-//     // Check if the search results container exists
-//     cy.get('.community-section-content').then($results => {
-//       if ($results.length > 0) {
-//         // Search results found
-//         cy.wrap($results).each(($result) => {
-//           cy.wrap($result).should('contain.text', search);
-//           cy.log('Search results found');
-//         });
-//       } else {
-//         // No search results found
-//         cy.log('No search results found for: ' + search);
-//       }
-//     });
-  
-//     // Clear the search input
-//     cy.get('#search-text').clear();
-  
-//     // Type another search term
-//     const newSearch = 'invisible';
-//     cy.get('#search-text')
-//       .type(newSearch)
-//       .should('have.value', newSearch);
-  
-//     // Check if "No Results" message is visible
-//     cy.get('#mat-tab-content-0-0').contains('No Results').should('be.visible');
-//   });
-  
-//   function generateRandomSearchTerm() {
-//     const possibleChars = 'abcdefghijklmnopqrstuvwxyz';
-//     const searchTermLength = 5;
-//     let searchTerm = '';
-//     for (let i = 0; i < searchTermLength; i++) {
-//       const randomIndex = Math.floor(Math.random() * possibleChars.length);
-//       searchTerm += possibleChars[randomIndex];
-//     }
-//     return searchTerm;
-//   } 
- });
+});
 
 
 
- it('should handle both existing and non-existing search terms correctly', () => {
-  cy.visit('/community');
+it('should handle both existing and non-existing search terms correctly', () => {
+cy.visit('/community');
 
-  const testCases = [
-      { term: 'This is a test feature idea', shouldExist: true },
-      { term: 'nonexistent search term', shouldExist: false }
+const testCases = [
+    { term: 'This is a test feature idea', shouldExist: true },
+    { term: 'nonexistent search term', shouldExist: false }
+];
+
+testCases.forEach(testCase => {
+  cy.intercept('POST', '**/squibler-api**').as('searchRequest');
+    // Replace '#search-text' with the actual selector for your search field
+    cy.get('#search-text').clear().type(testCase.term, { delay: 100 }).type('{enter}');
+    cy.wait('@searchRequest');
+
+    if (testCase.shouldExist) {
+        // Replace '.community-section-content' with the actual selector for the search results
+        cy.get('.community-section-content', { timeout: 10000 })
+            .contains(testCase.term)
+            .should('be.visible');
+    } else {
+        // Replace '#mat-tab-content-0-0' with the actual selector for the no results message
+        cy.get('#mat-tab-content-0-0', { timeout: 10000 })
+            .should('be.visible')
+            .and('contain', 'No Results.'); // Adjust the message text as necessary
+    }
+
+    // Adding a small wait time for search results to load before running the next test case
+    cy.wait(1000); // Adjust as necessary
+});
+});
+
+describe('Dropdown Filter Verification', () => {
+  const statuses = [
+      { status: 'Done', shouldExist: true },
+      { status: 'In Progress', shouldExist: true },
+      { status: 'Open', shouldExist: true },
+      
   ];
 
-  testCases.forEach(testCase => {
-      // Replace '#search-text' with the actual selector for your search field
-      cy.get('#search-text').clear().type(testCase.term, { delay: 100 }).type('{enter}');
+  statuses.forEach(({ status, shouldExist }) => {
+      it(`should filter results by ${status} status correctly`, () => {
+          // Visit the community page before each test case
+          cy.visit('/community');
 
-      if (testCase.shouldExist) {
-          // Replace '.community-section-content' with the actual selector for the search results
-          cy.get('.community-section-content', { timeout: 10000 })
-              .contains(testCase.term)
-              .should('be.visible');
-      } else {
-          // Replace '#mat-tab-content-0-0' with the actual selector for the no results message
-          cy.get('#mat-tab-content-0-0', { timeout: 10000 })
-              .should('be.visible')
-              .and('contain', 'No Results.'); // Adjust the message text as necessary
-      }
+          // Wait for the dropdown to be visible and then click
+          cy.get('#statusDropdown', { timeout: 10000 }).should('be.visible').click(); // Replace with the actual dropdown selector
+          cy.get('.mat-option-text').contains(status, { timeout: 10000 }).click(); // Replace with actual dropdown option selector
 
-      // Adding a small wait time for search results to load before running the next test case
-      cy.wait(1000); // Adjust as necessary
+          if (shouldExist) {
+              // Verify that results with the status flag are visible
+              cy.get('.community-post-status', { timeout: 10000 }).each(($el) => { // Replace with actual status selector
+                  cy.wrap($el)
+                      .invoke('text')
+                      .then(text => {
+                          expect(text.trim().toLowerCase()).to.eq(status.toLowerCase());
+                      });
+              });
+          } else {
+              // Verify that 'No Results' message is visible
+              cy.get('#mat-tab-content-0-0', { timeout: 10000 })
+            .should('be.visible')
+            .and('contain', 'No Results.'); // Replace with actual 'No Results' message selector
+          }
+          
+          // Adding a small wait time before running the next test case
+          cy.wait(1000); // Adjust as necessary
+      });
   });
 });
 
@@ -212,46 +202,6 @@ it.skip('should allow typing and display the search term and handle no results s
 
 
 
-  // } else {
-  //   // No results found
-  //   cy.log('No search results found');
-  //   // Add your assertions or actions for when no results are found
-  // }
-
-
-//   cy.visit('http://localhost:4200/community');
-  
-//   const search = 'test'; // Update the search term as needed
-  
-//   // Type the search term
-//   cy.get('#search-text')
-//     .should('be.visible')
-//     .clear() // Clear any existing text before typing
-//     .type(search)
-//     .should('have.value', search);
-
-//   // Check if any posts contain the search term
-//   cy.get('#mat-tab-content-0-0 > .mat-tab-body-content').then(() => {
-//     // Check if there are no posts
-//     if(cy.get('.community-post')){
-//       cy.get('.community-post').then(($posts) => {
-//         $posts.each((index, post) => {
-//           console.log(post)
-//         });
-//     });
-//     }else{
-//       cy.get('.community-section-content-posts-none').then(($noPosts) => {
-//           if ($noPosts.length > 0) {
-//               cy.contains('No Results').should('be.visible');
-//           } else {
-            
-//           }
-//       });
-//     }
-// });
-  
-  
-  
 
 
 
@@ -262,15 +212,9 @@ it.skip('should allow typing and display the search term and handle no results s
 
 
 
-
-
-
-
-
-        
-        
-        
-        
       
+      
+      
+      
+    
 
-  
